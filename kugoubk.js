931d5a55e811d9fc4f91813f391eb15fb37adf67
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-11 09:58:14 +0800

    👌

--- kugoubk.js
+++ kugoubk.js
@@ -1,38 +1,39 @@
+// COOKIE
 const envInstance = new Env("酷狗概念版自动领取VIP-Eric");
 
 if ($request.url) {
   let requestUrl = $request.url,
-      requestHeaders = $request.headers,
-      urlParams = new URLSearchParams(requestUrl.split("?")[1]),
-      parsedParams = {
-        "appid": urlParams.get("appid"),
-        "clientver": urlParams.get("clientver"),
-        "clienttime": urlParams.get("clienttime"),
-        "mid": urlParams.get("mid"),
-        "uuid": urlParams.get("uuid"),
-        "dfid": urlParams.get("dfid"),
-        "token": urlParams.get("token"),
-        "userid": urlParams.get("userid"),
-        "srcappid": urlParams.get("srcappid"),
-        "signature": urlParams.get("signature")
-      },
-      parsedHeaders = {
-        ":authority": requestHeaders[":authority"],
-        "content-type": requestHeaders["content-type"],
-        "kg-rf": requestHeaders["kg-rf"],
-        "accept": requestHeaders.accept,
-        "kg-thash": requestHeaders["kg-thash"],
-        "accept-language": requestHeaders["accept-language"],
-        "accept-encoding": requestHeaders["accept-encoding"],
-        "kg-rec": requestHeaders["kg-rec"],
-        "user-agent": requestHeaders["user-agent"],
-        "kg-rc": requestHeaders["kg-rc"],
-        "kg-fake": requestHeaders["kg-fake"],
-        "content-length": requestHeaders["content-length"],
-        "uni-useragent": requestHeaders["uni-useragent"]
-      };
+    requestHeaders = $request.headers,
+    urlParams = new URLSearchParams(requestUrl.split("?")[1]),
+    savedUrlParams = {
+      appid: urlParams.get("appid"),
+      clientver: urlParams.get("clientver"),
+      clienttime: urlParams.get("clienttime"),
+      mid: urlParams.get("mid"),
+      uuid: urlParams.get("uuid"),
+      dfid: urlParams.get("dfid"),
+      token: urlParams.get("token"),
+      userid: urlParams.get("userid"),
+      srcappid: urlParams.get("srcappid"),
+      signature: urlParams.get("signature")
+    },
+    savedHeaders = {
+      ":authority": requestHeaders[":authority"],
+      "content-type": requestHeaders["content-type"],
+      "kg-rf": requestHeaders["kg-rf"],
+      accept: requestHeaders.accept,
+      "kg-thash": requestHeaders["kg-thash"],
+      "accept-language": requestHeaders["accept-language"],
+      "accept-encoding": requestHeaders["accept-encoding"],
+      "kg-rec": requestHeaders["kg-rec"],
+      "user-agent": requestHeaders["user-agent"],
+      "kg-rc": requestHeaders["kg-rc"],
+      "kg-fake": requestHeaders["kg-fake"],
+      "content-length": requestHeaders["content-length"],
+      "uni-useragent": requestHeaders["uni-useragent"]
+    };
 
-  function saveData(key, value) {
+  function saveToStorage(key, value) {
     if (typeof $prefs !== "undefined") {
       return $prefs.setValueForKey(JSON.stringify(value), key);
     } else if (typeof $persistentStore !== "undefined") {
@@ -42,31 +43,30 @@ if ($request.url) {
     }
   }
 
-  saveData("urlParams", parsedParams);
-  saveData("headerParams", parsedHeaders);
-  saveData("originalUrl", requestUrl);
+  saveToStorage("urlParams", savedUrlParams);
+  saveToStorage("headerParams", savedHeaders);
+  saveToStorage("originalUrl", requestUrl);
 
-  envInstance.log("保存的 URL Params:", JSON.stringify(parsedParams));
-  envInstance.log("保存的 Header Params:", JSON.stringify(parsedHeaders));
+  envInstance.log("保存的 URL Params:", JSON.stringify(savedUrlParams));
+  envInstance.log("保存的 Header Params:", JSON.stringify(savedHeaders));
   envInstance.log("保存的 Original URL:", requestUrl);
-  envInstance.log("数据已保存到持久化存储 - Eric 为您提供");
+  envInstance.log("数据已保存到持久化存储 - Eric为您提供");
   envInstance.msg("获取数据成功", "数据已持久化保存", "");
   envInstance.done();
 }
 
-// 环境封装类
 function Env(name, options) {
-  class HTTPClient {
+  class HTTP {
     constructor(env) {
       this.env = env;
     }
     send(request, method = "GET") {
       request = typeof request === "string" ? { url: request } : request;
-      let handler = this.get;
-      if (method === "POST") handler = this.post;
+      let action = this.get;
+      if (method === "POST") action = this.post;
       return new Promise((resolve, reject) => {
-        handler.call(this, request, (err, response, body) => {
-          err ? reject(err) : resolve(response);
+        action.call(this, request, (error, response, body) => {
+          error ? reject(error) : resolve(response);
         });
       });
     }
@@ -78,17 +78,11 @@ function Env(name, options) {
     }
   }
 
-  return new (class {
+  return new class {
     constructor(name, options) {
       this.name = name;
-      this.http = new HTTPClient(this);
-      this.data = null;
-      this.dataFile = "box.dat";
+      this.http = new HTTP(this);
       this.logs = [];
-      this.isMute = false;
-      this.isNeedRewrite = false;
-      this.logSeparator = "\n";
-      this.encoding = "utf-8";
       this.startTime = new Date().getTime();
       Object.assign(this, options);
       this.log("", `🔔${this.name}, 开始!`);
@@ -106,47 +100,116 @@ function Env(name, options) {
       return undefined;
     }
 
+    isNode() {
+      return this.getEnv() === "Node.js";
+    }
+
+    isQuanX() {
+      return this.getEnv() === "Quantumult X";
+    }
+
+    isSurge() {
+      return this.getEnv() === "Surge";
+    }
+
+    isLoon() {
+      return this.getEnv() === "Loon";
+    }
+
+    isShadowrocket() {
+      return this.getEnv() === "Shadowrocket";
+    }
+
+    isStash() {
+      return this.getEnv() === "Stash";
+    }
+
     log(...messages) {
-      if (messages.length > 0) {
-        this.logs = [...this.logs, ...messages];
-        console.log(messages.join(this.logSeparator));
-      }
+      this.logs = [...this.logs, ...messages];
+      console.log(messages.join("\n"));
     }
 
-    logErr(err) {
-      this.log("", `❗️${this.name}, 错误!`, err.stack || err);
-    }
-
-    msg(title = "", subtitle = "", message = "", options = {}) {
-      if (!this.isMute) {
-        const formattedOptions = this.formatOptions(options);
-        switch (this.getEnv()) {
-          case "Surge":
-          case "Loon":
-          case "Shadowrocket":
-          case "Stash":
-            $notification.post(title, subtitle, message, formattedOptions);
-            break;
-          case "Quantumult X":
-            $notify(title, subtitle, message, formattedOptions);
-            break;
-          default:
-            console.log(`${title}\n${subtitle}\n${message}`);
-        }
-      }
-    }
-
-    formatOptions(options) {
-      if (typeof options === "string") return { "open-url": options };
-      if (typeof options === "object") return options;
-      return {};
+    msg(title = "", subtitle = "", body = "") {
+      console.log(`${title}\n${subtitle}\n${body}`);
     }
 
     done() {
-      const elapsedTime = (new Date().getTime() - this.startTime) / 1000;
-      this.log("", `🔔${this.name}, 结束! 🕛 ${elapsedTime} 秒`);
-      this.log();
-      if (this.getEnv() === "Node.js") process.exit();
+      const endTime = new Date().getTime();
+      const duration = (endTime - this.startTime) / 1000;
+      this.log("", `🔔${this.name}, 结束! 🕛 ${duration} 秒`);
     }
-  })(name, options);
+
+    get(request, callback = () => {}) {
+      switch (this.getEnv()) {
+        case "Surge":
+        case "Loon":
+        case "Stash":
+        case "Shadowrocket":
+          $httpClient.get(request, (error, response, body) => {
+            callback(error, response, body);
+          });
+          break;
+        case "Quantumult X":
+          request.method = "GET";
+          $task.fetch(request).then(
+            (response) => {
+              callback(null, response, response.body);
+            },
+            (error) => {
+              callback(error);
+            }
+          );
+          break;
+        case "Node.js":
+          const got = require("got");
+          got(request).then(
+            (response) => {
+              callback(null, response, response.body);
+            },
+            (error) => {
+              callback(error);
+            }
+          );
+          break;
+      }
+    }
+
+    post(request, callback = () => {}) {
+      switch (this.getEnv()) {
+        case "Surge":
+        case "Loon":
+        case "Stash":
+        case "Shadowrocket":
+          $httpClient.post(request, (error, response, body) => {
+            callback(error, response, body);
+          });
+          break;
+        case "Quantumult X":
+          request.method = "POST";
+          $task.fetch(request).then(
+            (response) => {
+              callback(null, response, response.body);
+            },
+            (error) => {
+              callback(error);
+            }
+          );
+          break;
+        case "Node.js":
+          const got = require("got");
+          got.post(request.url, {
+            headers: request.headers,
+            body: request.body,
+          }).then(
+            (response) => {
+              callback(null, response, response.body);
+            },
+            (error) => {
+              callback(error);
+            }
+          );
+          break;
+      }
+    }
+  }(name, options);
 }
